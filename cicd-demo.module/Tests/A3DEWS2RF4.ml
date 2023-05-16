3609fb9d6dd6fed4bbe49edc3bb23c00906261d2
mpandav <mpandav32@gmail.com>
2023-05-16 14:25:41 +0200

    Added unit test to app

---
+++ cicd-demo.module/Tests/A3DEWS2RF4.ml
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="ASCII"?>
+<emulation:BWTFile xmi:version="2.0" xmlns:xmi="http://www.omg.org/XMI" xmlns:emulation="http:///emulation.ecore">
+  <CurrentLoadedTest href="platform:/resource/cicd-demo.module/Tests/cicd-demo.module1.bwt#/"/>
+  <EmulationDataFile>ACED0005740015636963642D64656D6F2E6D6F64756C65312E627774</EmulationDataFile>
+</emulation:BWTFile>
